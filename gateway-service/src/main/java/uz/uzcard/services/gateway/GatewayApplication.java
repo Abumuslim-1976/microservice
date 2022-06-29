34bfc472d3bc47c523f49afac39f5e3d073d4da2
Abumuslim <amonovabumuslim@gmail.com>
2022-06-29 18:23:43 +0500

    config service removed

--- gateway-service/src/main/java/uz/uzcard/services/gateway/GatewayApplication.java
+++ gateway-service/src/main/java/uz/uzcard/services/gateway/GatewayApplication.java
@@ -1,17 +1,9 @@
 package uz.uzcard.services.gateway;
 
-import org.springdoc.core.GroupedOpenApi;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.cloud.gateway.route.RouteDefinition;
-import org.springframework.cloud.gateway.route.RouteDefinitionLocator;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
-import org.springframework.context.annotation.Bean;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @SpringBootApplication
 @EnableDiscoveryClient
